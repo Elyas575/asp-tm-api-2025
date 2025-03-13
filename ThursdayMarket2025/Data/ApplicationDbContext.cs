2d97b40a0a4f9b50a5fa8e43f45c64cd9c70f0f5
Elyas <lirzqad@gmail.com>
2025-03-13 13:46:45 +0200

    migrations added

---
+++ ThursdayMarket2025/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThursdayMarket2025.Models;
+
+namespace ThursdayMarket2025.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
