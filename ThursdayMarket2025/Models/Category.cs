fb893802d004cc43f7dc08b25cf8d697ec63fd31
Elyas <lirzqad@gmail.com>
2025-03-13 12:56:21 +0200

    category entity field removed

--- ThursdayMarket2025/Models/Category.cs
+++ ThursdayMarket2025/Models/Category.cs
@@ -7,7 +7,6 @@ namespace ThursdayMarket2025.Models
         [Key]
         public int Id { get; set; } 
         public string Name { get; set; }
-        public string Description { get; set; }
         public string displayOrder { get; set; }
         public DateTime? CreatedDate { get; set; } = DateTime.Now;
         public DateTime? UpdatedDate { get; set; }
