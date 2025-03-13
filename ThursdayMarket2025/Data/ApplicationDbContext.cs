16968c065da60881e1852770eac0186486d52763
Elyas <lirzqad@gmail.com>
2025-03-13 14:22:11 +0200

    category entity updated

--- ThursdayMarket2025/Data/ApplicationDbContext.cs
+++ ThursdayMarket2025/Data/ApplicationDbContext.cs
@@ -10,5 +10,13 @@ namespace ThursdayMarket2025.Data
             
         }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Category>().HasData(
+            //        new Category { Id=1, Name= "Fruits", DisplayOrder = 1, },
+            //        new Category { Id=2, Name=" Vegetables", DisplayOrder=1 }
+            //    );
+        }
     }
 }
--- ThursdayMarket2025/Models/Category.cs
+++ ThursdayMarket2025/Models/Category.cs
@@ -7,7 +7,7 @@ namespace ThursdayMarket2025.Models
         [Key]
         public int Id { get; set; } 
         public string Name { get; set; }
-        public string displayOrder { get; set; }
+        public int DisplayOrder { get; set; }
         public DateTime? CreatedDate { get; set; } = DateTime.Now;
         public DateTime? UpdatedDate { get; set; }
     }
